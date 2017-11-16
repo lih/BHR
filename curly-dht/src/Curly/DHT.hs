595752f3f43d3146db5ae4b711a36977d6f9d92f
Marc Coiffier <marc@coiffier.net>
2017-11-16 21:30:34 +0100

    Add a 'curly-dht' package

---
+++ curly-dht/src/Curly/DHT.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE DeriveGeneric, TypeFamilies, ScopedTypeVariables, PatternSynonyms, ViewPatterns #-}
+module Curly.DHT(
+  main
+  ) where
+
+import Definitive
+import Language.Format
+import System.Environment (getArgs)
+import Curly.DHT.Kademlia
+import Curly.Core.Security
+import Curly.Core.Library
+import Curly.Core.VCS
+import IO.Network.Socket
+import Control.Concurrent (forkIO)
+import Control.Concurrent.MVar
+
+data Key = NodeKey String
+         | DataKey ValID
+         | LibraryKey LibraryID (WithResponse Bytes)
+         | SourceKey LibraryID (WithResponse (Signed String))
+         | BranchesKey PublicKey (WithResponse (Signed Branches))
+         | CommitKey Hash (WithResponse Commit)
+         deriving (Show,Generic)
+instance Serializable Key ; instance Format Key
+instance Eq Key where a==b = compare a b==EQ
+instance Ord Key where compare = comparing serialize
+instance DHTIndex Key
+
+newtype ValID = ValID Hash
+              deriving (Eq,Ord,Show,Generic)
+instance Serializable ValID; instance Format ValID
+
+data Val = PartialVal [ValID]
+         | DataVal Bytes
+         deriving (Show,Generic)
+instance Serializable Val ; instance Format Val
+instance Eq Val where a==b = compare a b==EQ
+instance Ord Val where compare = comparing serialize
+instance DHTValue Val
+
+valID :: Val -> ValID
+valID = ValID . hashData . serialize
+
+parMap :: Traversable t => (a -> IO b) -> t a -> IO (t b)
+parMap k ta = for ta $ \a -> do
+  v <- newEmptyMVar
+  forkIO (k a >>= putMVar v)
+  return (readMVar v^.thunk)
+
+insertMPBytes :: DHTInstance Key Val -> Bytes -> IO ValID
+insertMPBytes dht b
+  | bytesSize b<=256 = let v = DataVal b ; cid = valID v in insertDHT dht (DataKey cid) v >> return cid
+  | otherwise = let parts = mapAccum_ (\n b -> swap $ splitAt n b) (take 8 $ repeat ((bytesSize b + 7) `div` 8)) b 
+                in parMap (insertMPBytes dht) parts >>= \ps -> let v = PartialVal ps ; cid = valID v
+                                                               in cid `seq` insertDHT dht (DataKey cid) v >> return cid
+
+lookupMPBytes :: DHTInstance Key Val -> Key -> IO (Maybe Bytes)
+lookupMPBytes dht k = do
+  x <- lookupDHT dht k
+  case x of
+    Just (DataVal b,_) -> return (Just b)
+    Just (PartialVal ks,_) -> do
+      parts <- parMap (lookupMPBytes dht . DataKey) ks
+      return (fold <$> sequence parts)
+    Nothing -> return Nothing
+
+lookupMP :: (Format a,MonadIO m) => DHTInstance Key Val -> (WithResponse a -> Key) -> m (Maybe a)
+lookupMP dht fk = liftIO $ lookupMPBytes dht (fk WithResponse) <&> (>>= matches Just datum)
+insertMP :: (Serializable a,MonadIO m) => DHTInstance Key Val -> (WithResponse a -> Key) -> a -> m ()
+insertMP dht fk a = liftIO $ insertMPBytes dht (serialize a) >>= \cid -> insertDHT dht (fk WithResponse) (PartialVal [cid])
+
+isValidAssoc (DataKey h) v = valID v == h
+isValidAssoc _ _ = True
+
+main = do
+  args <- getArgs
+  case args of
+    (Readable (fromInteger -> serverport):args') -> do
+      let isRoot = empty args'
+          dhtport = if isRoot then 25464 else fromMaybe (error "No parse for DHT port (expected number)")
+                                              (matches Just readable (head args'))
+      dht <- newDHTInstance dhtport (NodeKey ("me "+show dhtport)) isValidAssoc
+      unless isRoot $ do
+        res <- joinDHT dht (DHTNode "127.0.0.1" 25464 (NodeKey ("me "+show 25464)))
+        case res of
+          JoinSucces -> putStrLn "Successfully joined network node 127.0.0.1:25464"
+          _ -> error "Couldnt't reach root node"
+      sock <- listenOn serverport
+      forever $ do
+        (h,_) <- accept sock
+        void $ forkIO $ runConnection_ True h $ forever $ do
+          x <- receive
+          case x of
+            PublishLibrary lid l -> do
+              insertMP dht (LibraryKey lid) l
+            PublishSource lid s -> do
+              insertMP dht (SourceKey lid) s
+            GetLibrary l t -> do
+              sending t =<< lookupMP dht (LibraryKey l)
+            GetSource lid t -> do
+              sending t =<< lookupMP dht (SourceKey lid)
+            SetBranches pub bs -> do
+              insertMP dht (BranchesKey pub) bs
+            CreateCommit c t -> do
+              let h = hashData (serialize c)
+              insertMP dht (CommitKey h) c
+              sending t h
+            GetCommit h t -> do
+              sending t =<< lookupMP dht (CommitKey h)
+            ListBranches pub t -> do
+              sending t =<< lookupMP dht (BranchesKey pub)
+    _ -> putStrLn "Usage: curly-dht <server-port> [<dht-port>]"
+
+pattern Readable a <- (matches Just readable -> Just a)
---
+++ curly-dht/src/Curly/DHT/Kademlia.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DeriveGeneric, TypeFamilies, ScopedTypeVariables, PatternSynonyms #-}
+module Curly.DHT.Kademlia (
+  DHTIndex,DHTValue,
+  DHTInstance,DHTNode,pattern DHTNode,
+  JoinResult(..),newDHTInstance,closeDHTInstance,joinDHT,lookupDHT,insertDHT,
+  ) where
+
+import Definitive
+import Language.Format
+import qualified Network.Kademlia as K
+import Network.Kademlia (JoinResult(..))
+
+newtype K a = K { getK :: a }
+            deriving (Eq,Ord)
+instance K.Serialize a => Serializable (K a) where
+  encode (K a) = encode (K.toBS a^..chunk)
+instance K.Serialize a => Format (K a) where
+  datum = datum >>= (const zero <|> return) . map (K . fst) . K.fromBS . by chunk
+instance Format a => K.Serialize (K a) where
+  fromBS b = case (datum^..parser) (b^..chunk) of
+    (s,a):_ -> Right (K a,s^.chunk)
+    [] -> Left "Parse error"
+  toBS (K a) = serialize a^.chunk
+
+class (Ord a,Format a) => DHTIndex a
+class (Eq a,Format a) => DHTValue a
+newtype DHTInstance i a = DHTInstance { _getDHTInstance :: K.KademliaInstance (K i) (K a) }
+newtype DHTNode i = DHTNodeImpl { _getDHTNode :: K.Node (K i) }
+pattern DHTNode h p i = DHTNodeImpl (K.Node (K.Peer h p) (K i))
+
+newDHTInstance :: (DHTIndex i,DHTValue a,MonadIO m) => Int -> i -> (i -> a -> Bool) -> m (DHTInstance i a)
+newDHTInstance p i integ = liftIO $ DHTInstance <$> K.create p (K i) (\(K i) (K a) -> integ i a)
+closeDHTInstance :: MonadIO m => DHTInstance i a -> m ()
+closeDHTInstance (DHTInstance i) = liftIO $ K.close i
+joinDHT :: (DHTIndex i,DHTValue a,MonadIO m) => DHTInstance i a -> DHTNode i -> m JoinResult
+joinDHT (DHTInstance i) (DHTNodeImpl n) = liftIO $ K.joinNetwork i n
+lookupDHT :: (DHTIndex i,DHTValue a,MonadIO m) => DHTInstance i a -> i -> m (Maybe (a,DHTNode i))
+lookupDHT (DHTInstance i) k = liftIO $ K.lookup i (K k) <&> map (getK <#> DHTNodeImpl)
+insertDHT :: (DHTIndex i,DHTValue a,MonadIO m) => DHTInstance i a -> i -> a -> m ()
+insertDHT (DHTInstance i) k a = liftIO $ K.store i (K k) (K a)
