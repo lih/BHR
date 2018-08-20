94efcfa35db3b818763810a9649749a123e49f8d
Marc Coiffier <marc.coiffier@gmail.com>
2018-08-20 17:46:10 +0200

    Update the CI config

---
+++ scripts/ci/build-package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+stack_path="$(stack path --local-install-root)"
+
+for exe; do
+    version="$(sed -n 's/^version:\s*//p' "$exe/$exe.cabal")"
+    full="$exe-$version"
+    shopt -s nullglob
+    (
+	tmp="$(mktemp -d)"
+	mkdir -p "$tmp/$full"
+	cd "$stack_path"
+	cp -r bin/$exe share/*/$exe-*/* "$tmp/$full"
+	tar -C "$tmp" -c .
+	rm -r "$tmp"
+    ) | xz > "$exe.tar.xz"
+done
