e312274bb4fef38e4ac180f45bca67d86e1251f9
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2018-11-10 14:11:08 +0100

    Successful build (on Sat Nov 10 14:11:07 CET 2018)

--- logos/exe/Logos.hs
+++ logos/exe/Logos.hs
@@ -3,4 +3,22 @@ module Main where
 import Definitive
 import Algebra.Monad.Concatenative
 
-main = putStrLn "Hello from Logos !"
+stringWords :: String -> [String]
+stringWords = map fromString . fromBlank
+  where fromBlank (c:t) | c `elem` [' ', '\t', '\r', '\n'] = fromBlank t
+                        | c == '"' = fromQuote id t
+                        | otherwise = fromWChar (c:) t
+        fromBlank "" = []
+        fromQuote k ('"':t) = ('"':k "\""):fromBlank t
+        fromQuote k ('\\':c:t) = fromQuote (k.(qChar c:)) t
+          where qChar 'n' = '\n' ; qChar 't' = '\t' ; qChar x = x
+        fromQuote k (c:t) = fromQuote (k.(c:)) t
+        fromQuote k "" = ['"':k "\""]
+        fromWChar k (c:t) | c `elem` [' ', '\t', '\r', '\n'] = k "":fromBlank t
+                          | otherwise = fromWChar (k.(c:)) t
+        fromWChar k "" = [k ""]
+
+main = do
+  text <- readHString stdin
+  
+  putStrLn "Hello from Logos !"
