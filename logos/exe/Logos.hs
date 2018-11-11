c09f7cb1e5f787cc6e388dd3ac01ada7465a86f4
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2018-11-11 14:21:28 +0100

    Successful build (on Sun Nov 11 14:21:28 CET 2018)

--- logos/exe/Logos.hs
+++ logos/exe/Logos.hs
@@ -150,15 +150,15 @@ runLogos Texture = do
     StackSymbol file:st' -> do
       runStackState (put st')
       textureLoaded <- liftIO $ do
-        tex <- GL.genObjectName
-        GL.textureBinding GL.Texture2D SV.$= Just tex
         imgbytes <- readChunk file
         let img = convertRGB8 <$> decodeImage imgbytes
+        tex <- GL.genObjectName
         case img of
           Right (Image w h imgd) -> do
+            GL.textureBinding GL.Texture2D SV.$= Just tex
             V.unsafeWith imgd $ \imgp -> do
               GL.texImage2D GL.Texture2D GL.NoProxy 0 GL.RGBA8 (GL.TextureSize2D (fromIntegral w) (fromIntegral h)) 0 (GL.PixelData GL.BGR GL.UnsignedByte imgp)
-            GL.textureFilter GL.Texture2D SV.$= ((GL.Nearest,Nothing),GL.Nearest)
+            GL.textureFilter GL.Texture2D SV.$= ((GL.Linear',Nothing),GL.Linear')
             GL.generateMipmap' GL.Texture2D
             GL.textureBinding GL.Texture2D SV.$= Nothing
             return $ Just tex
