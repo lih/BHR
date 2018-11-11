c5524f95b3ef9a396763710c7607713d2eafde5c
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2018-11-11 15:33:55 +0100

    Successful build (on Sun Nov 11 15:33:55 CET 2018)

--- logos/exe/Logos.hs
+++ logos/exe/Logos.hs
@@ -206,7 +206,7 @@ main = do
   between (void GLFW.initialize) GLFW.terminate $ do
     args <- getArgs
   
-    GLFW.openWindowHint GLFW.OpenGLProfile GLFW.OpenGLCoreProfile
+    GLFW.openWindowHint GLFW.OpenGLProfile GLFW.OpenGLCompatProfile
     
     GL.depthFunc            SV.$= Just GL.Lequal
     GL.blend                SV.$= GL.Enabled
