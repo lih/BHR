f369e252203708759b2878fbac856204a3bcccc1
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2018-12-03 23:14:57 +0100

    Successful build (on Mon Dec  3 23:14:57 CET 2018)

--- capricon/exe/WiQEE.hs
+++ capricon/exe/WiQEE.hs
@@ -90,7 +90,7 @@ setBytes :: String -> [Word8] -> JS.CIO ()
 setBytes f v = setString f (map (toEnum . fromIntegral) v)
 
 hasteDict :: COCDict JS.CIO String
-hasteDict = cocDict ("0.9.1-js" :: String) getString getBytes setString setBytes
+hasteDict = cocDict ("0.9.2-js" :: String) getString getBytes setString setBytes
 
 main :: IO ()
 main = JS.concurrent $ void $ do
