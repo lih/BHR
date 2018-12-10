fffcb62f78af7590fdb176e9f8a2b407548210af
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2018-12-10 16:35:49 +0100

    Successful build (on Mon Dec 10 16:35:48 CET 2018)

--- logos/src/Data/Matricial.hs
+++ logos/src/Data/Matricial.hs
@@ -72,6 +72,8 @@ matMult x y = map (\vm -> map (\vm' -> scalProd vm vm') (transpose y)) x
 
 scalProd :: (Ring a,Vector (Vec n)) => Vec n a -> Vec n a -> a
 scalProd u v = sum (u*v)
+scalProdM :: (Ring a,Vector (Vec n)) => Mat n n a -> Mat n n a -> Mat n n a
+scalProdM u v = fromLinearMap (from scalar %~ \w -> (w $* u) * (w $* v))
 
 normalize :: (Vector (Vec n),Invertible a, RealFloat a) => Vec n a -> Vec n a
 normalize v = map (/ sqrt (scalProd v v)) v
