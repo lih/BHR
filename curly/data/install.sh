e5d026b25471afb5194a6bbc57de765aca4b4197
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2019-03-04 22:24:36 +0100

    Successful build (on Mon Mar  4 22:24:36 CET 2019)

--- curly/data/install.sh
+++ curly/data/install.sh
@@ -26,7 +26,7 @@ EOF
 
     bash-completions)
 	data="${XDG_DATA_HOME:-$HOME/.local/share}/bashcomps/"
-	mkdir -p "$data"
+	mkdir -p "$data/completions"
 	get_data "$data/bashcomps.shl"			bash/completions/bashcomps.shl
 	get_data "$data/completions/curly"		bash/completions/curly
 	get_data "$data/completions/curly.arg.shf"	bash/completions/curly.arg.shf
