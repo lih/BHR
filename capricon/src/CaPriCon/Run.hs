afb745c11aae14d0501a244d7b884950b1cb0a6e
Marc Coiffier <marc.coiffier@univ-grenoble-alpes.fr>
2019-04-07 01:50:52 +0200

    Successful build (on Sun Apr  7 01:50:51 CEST 2019)

--- capricon/src/CaPriCon/Run.hs
+++ capricon/src/CaPriCon/Run.hs
@@ -551,6 +551,6 @@ outputComment c = (runExtraState $ do outputText =~ (\o t -> o (commentText+t)))
              "<span class=\"expand-else capricon-show\"></span>"+
              "<span class=\"expand-then capricon-hide\"></span>"+
              "<span class=\"expand-then"+(if hasExamples then " capricon-with-examples" else "")+"\" style=\"--num-lines: "
-             + fromString (show (1.25 + (if hasExamples then 1.25 else 0) + fromIntegral nlines :: Float))+"\">"
+             + fromString (show ((if isP then 1.25 + (if hasExamples then 1.25 else 0) else 0) + fromIntegral nlines :: Float))+"\">"
         wrapEnd = "</span></label>"
   
